0e447625fe2744672b8dac7a6837f04524045334
Alice Lee <alicejeeuncodes@gmail.com>
2022-11-10 23:19:58 +0000

    API login route setup. #1

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ require('dotenv/config');
 const pg = require('pg');
 const express = require('express');
 const argon2 = require('argon2');
+const jwt = require('jsonwebtoken');
 const ClientError = require('./client-error');
 const errorMiddleware = require('./error-middleware');
 
@@ -37,6 +38,40 @@ app.post('/registration', (req, res, next) => {
     .then(result => {
       const [user] = result.rows;
       res.status(201).json(user);
+    })
+    .catch(err => next(err));
+});
+
+app.post('/login', (req, res, next) => {
+  const { email, password } = req.body;
+  if (!email || !password) {
+    throw new ClientError(401, 'invalid login');
+  }
+  const sql = `
+    SELECT "userId",
+           "hashedPassword",
+           "email"
+    FROM "users"
+    WHERE "email" = $1
+  `;
+  const params = [email];
+  db.query(sql, params)
+    .then(result => {
+      const [user] = result.rows;
+      if (!user) {
+        throw new ClientError(401, 'invalid login');
+      }
+      const { userId, hashedPassword } = user;
+      return argon2
+        .verify(hashedPassword, password)
+        .then(isMatching => {
+          if (!isMatching) {
+            throw new ClientError(401, 'invalid login');
+          }
+          const payload = { userId, email };
+          const token = jwt.sign(payload, process.env.TOKEN_SECRET);
+          res.json({ token, user: payload });
+        });
     })
     .catch(err => next(err));
 });
